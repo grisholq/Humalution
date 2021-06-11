f30bb7348b155a42d743101cf7173151f00e9243
grisholq <drowen2014@mail.ru>
2021-06-11 21:41:33 +0300

    Commit 11.06

---
+++ Assets/Scripts/Map/TilesMapGenerator.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TilesMapGenerator : MonoBehaviour
+{
+    [SerializeField] private int width;
+    [SerializeField] private int height;
+
+    [SerializeField] private List<TileTerrainData>  tileTerrains;
+
+    private void Awake()
+    {
+        GenerateMap();
+    }
+
+    private Transform GenerateMap()
+    {
+        Transform map = new GameObject("TilesMap").transform;
+
+        Vector3 start = new Vector3(-width / 2, 0, -height / 2) ;
+
+        for (int i = 0; i < width; i++)
+        {
+            for (int i1 = 0; i1 < height; i1++)
+            {
+                Transform tile = CreateTile(tileTerrains.Random());
+                tile.position = start + new Vector3(i, 0, i1);
+            }
+        }
+
+        return map;
+    }
+
+    private Transform CreateTile(TileTerrainData terrainData)
+    {
+        Transform tile = new GameObject("Tile").transform;
+
+        Transform tileBase = Instantiate(terrainData.TileBase, tile);
+        Transform tileContent = Instantiate(terrainData.TileContent, tile);
+
+        return tile;
+    }
+}
---
+++ Assets/Scripts/Tile/Terrain/TileTerrainData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+[CreateAssetMenu(fileName = "TerrainData", menuName = "MyAssets/TerrainData")]
+public class TileTerrainData : ScriptableObject
+{
+    [SerializeField] private Transform terrainBase;
+    [SerializeField] private Transform terrainContent;
+
+    public Transform TileBase { get => terrainBase; }
+    public Transform TileContent { get => terrainContent; }
+}
